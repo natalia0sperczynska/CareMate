1ddcb73d4da567a93e3b29f481875d10fc740d6f
nsperczynska <278444@student.pwr.edu.pl>
2025-01-03 14:00:44 +0100

    update UI main user data update i to ostatnie nie pamietam

--- app/src/main/java/com/example/myfirstapp/MainUser.kt
+++ app/src/main/java/com/example/myfirstapp/MainUser.kt
@@ -1,9 +1,11 @@
 package com.example.myfirstapp
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.LinearLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +13,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import com.example.myfirstapp.firebase.FireStore
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.auth.User
 import kotlinx.coroutines.launch
 
 class MainUser : AppCompatActivity() {
@@ -18,43 +21,68 @@ class MainUser : AppCompatActivity() {
     private val auth = FirebaseAuth.getInstance() // Firebase authentication instance
     private val firestoreClass = FireStore() // Firestore interaction helper class
 
-   // @SuppressLint("MissingInflatedId")
+
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //enableEdgeToEdge()
         setContentView(R.layout.activity_main_user)
 
        val userId=auth.currentUser?.uid
 
+        val welcomeText: TextView = findViewById(R.id.nameAndSurname)
 
        findViewById<TextView>(R.id.message).text = "Welcome!"
+
        if (userId != null) {
-           loadUserData(userId)
+           loadUserData(userId,welcomeText)
        }
-       val logout: Button = findViewById(R.id.logoutButton)
+       //val logout1: Button = findViewById(R.id.logoutButton)
+        val logout: LinearLayout = findViewById(R.id.logout)
        logout.setOnClickListener {
            FirebaseAuth.getInstance().signOut() // Sign out the user
            val intent = Intent(this, MainActivity::class.java)
            startActivity(intent)
            finish() // Prevent returning to this activity when back is pressed
        }
-       val updateData: Button = findViewById(R.id.updatedataButton)
+      // val updateData: Button = findViewById(R.id.updatedataButton)
+        val updateData: LinearLayout = findViewById(R.id.updateData)
+
 
       updateData.setOnClickListener {
            val intent = Intent(this, UpdateDataActivity::class.java)
            startActivity(intent)
        }
 
+//        val notificationsButton: LinearLayout = findViewById(R.id.imageView12).parent
+//        notificationsButton.setOnClickListener {
+//            val intent = Intent(this, NotificationsActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        val calendarButton: LinearLayout = findViewById(R.id.imageView13).parent
+//        calendarButton.setOnClickListener {
+//            val intent = Intent(this, CalendarActivity::class.java)
+//            startActivity(intent)
+//        }
+//
+//        val appointmentButton: LinearLayout = findViewById(R.id.imageView14).parent
+//        appointmentButton.setOnClickListener {
+//            val intent = Intent(this, SetAppointmentActivity::class.java)
+//            startActivity(intent)
+//        }
+
    }
 
 
-    private fun loadUserData(userId: String) {
+    private fun loadUserData(userId: String,welcomeText: TextView) {
         lifecycleScope.launch {
             try {
                 // Fetch user data from Firestore
                 val data = firestoreClass.loadUserData(userId)
                 if (data != null) {
-                    Toast.makeText(this@MainUser, "\"Welcome back!", Toast.LENGTH_SHORT).show()
+                    val userName=data.getValue("name")
+                    welcomeText.text = "Good to see you again $userName!"
+                    Toast.makeText(this@MainUser, "Welcome back!", Toast.LENGTH_SHORT).show()
                 } else {
                     Toast.makeText(this@MainUser, "No user data found.", Toast.LENGTH_SHORT).show()
                 }
