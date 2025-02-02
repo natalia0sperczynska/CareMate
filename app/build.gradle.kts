1e7b433271e9269d61539697092125fc2c11f667
nsperczynska <278444@student.pwr.edu.pl>
2025-02-02 16:29:38 +0100

    commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.myfirstapp"
-        minSdk = 34
+        minSdk = 31
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
