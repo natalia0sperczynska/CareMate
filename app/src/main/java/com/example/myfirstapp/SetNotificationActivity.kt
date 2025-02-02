a85d5662708ace2fdcad3bfb734ca956865aa86d
nsperczynska <278444@student.pwr.edu.pl>
2025-02-02 12:38:15 +0100

    commit

--- app/src/main/java/com/example/myfirstapp/SetNotificationActivity.kt
+++ app/src/main/java/com/example/myfirstapp/SetNotificationActivity.kt
@@ -1,12 +1,13 @@
 package com.example.myfirstapp
 
+import android.annotation.SuppressLint
 import android.app.AlarmManager
-import android.app.PendingIntent
 import android.app.TimePickerDialog
-import android.content.Context
 import android.content.Intent
-import android.content.SharedPreferences
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
@@ -17,19 +18,21 @@ import java.util.Calendar
 
 
 class SetNotificationActivity : AppCompatActivity() {
+    private lateinit var morningTimeButton :Button
+    private lateinit var eveningTimeButton: Button
+    private lateinit var goBack: Button
+    private lateinit var clear: Button
     private lateinit var alarmScheduler: AlarmScheduler
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        requestExactAlarmPermission()
+
         setContentView(R.layout.activity_set_notification)
 
         alarmScheduler = AlarmSchedulerImpl(this)
 
-
-        val morningTimeButton: Button = findViewById(R.id.morningTimeButton)
-        val eveningTimeButton: Button = findViewById(R.id.eveningTimeButton)
-        val goBack: Button = findViewById(R.id.gobackbutton1)
-        val clear : Button = findViewById(R.id.clear)
+        setup()
 
         morningTimeButton.setOnClickListener {
             showTimePicker { hour, minute ->
@@ -66,6 +69,19 @@ class SetNotificationActivity : AppCompatActivity() {
         }
 
     }
+
+    @SuppressLint("ObsoleteSdkInt")
+    private fun requestExactAlarmPermission() {
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) return
+
+        val alarmManager = getSystemService(AlarmManager::class.java)
+        if (!alarmManager.canScheduleExactAlarms()) {
+            startActivity(Intent(ACTION_REQUEST_SCHEDULE_EXACT_ALARM).apply {
+                data = Uri.parse("package:$packageName")
+            })
+        }
+    }
+
     private fun showTimePicker(onTimeSelected: (hour: Int, minute: Int) -> Unit) {
         val calendar = Calendar.getInstance()
         val hour = calendar.get(Calendar.HOUR_OF_DAY)
@@ -75,5 +91,10 @@ class SetNotificationActivity : AppCompatActivity() {
             onTimeSelected(selectedHour, selectedMinute)
         }, hour, minute, true).show()
     }
-
+    private fun setup(){
+        morningTimeButton = findViewById(R.id.morningTimeButton)
+        eveningTimeButton = findViewById(R.id.eveningTimeButton)
+        goBack = findViewById(R.id.gobackbutton1)
+        clear = findViewById(R.id.clear)
+    }
 }
